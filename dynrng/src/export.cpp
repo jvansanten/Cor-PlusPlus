f31425310c5e5d272fcd8d534b2ea77cc9f21f32
Jakob van Santen <jvansanten@gmail.com>
2018-09-26 22:56:53 +0200

    Add Threefry4x64 engine from Random123

--- dynrng/src/export.cpp
+++ dynrng/src/export.cpp
@@ -13,8 +13,21 @@
 #include <random>
 #include <sstream>
 
+#ifdef __RNG_RANDOM123__
+#include "Random123/threefry.h"
+#include "Random123/MicroURNG.hpp"
+typedef r123::MicroURNG<r123::Threefry4x64> RNGType;
+std::array<RNGType, 6> generators = {
+	RNGType({{}},{{}}), // gruuuh dirty
+	RNGType({{}},{{}}),
+	RNGType({{}},{{}}),
+	RNGType({{}},{{}}),
+	RNGType({{}},{{}}),
+	RNGType({{}},{{}})
+};
+#else
 std::array<std::mt19937_64, 6> generators;
-
+#endif
 
 extern "C" void dynrng_seed_(unsigned int *gen, unsigned int* seeds)
 {
@@ -23,8 +36,15 @@ extern "C" void dynrng_seed_(unsigned int *gen, unsigned int* seeds)
 		oss << "RNG stream " << *gen << " out of range";
 		throw std::out_of_range(oss.str().c_str());
 	}
+#ifdef __RNG_RANDOM123__
+	// in default mode, only use the lower 32 bits for key and lower 64 for counter
+	RNGType::key_type key = {{0,0,0,seeds[0]}};
+	RNGType::ctr_type counter = {{0,0,0,seeds[1]+1000000000*size_t(seeds[2])}};
+	generators[*gen-1].reset(counter, key);
+#else
 	generators[*gen-1].seed(seeds[0]);
-	generators[*gen-1].discard(seeds[1] + 1000000000*size_t(seeds[2]));
+	generators[*gen-1].discard(seeds[1]+1000000000*size_t(seeds[2]));
+#endif
 }
 
 extern "C" void dynrng_getrandom_(unsigned int* gen, int* N, double* numbers)
